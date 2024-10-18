2ae7926bc0e58cf49f66aaae44648cea41f4f77d
LOST HACKER <96609972+LOSTHACKEROFC@users.noreply.github.com>
2024-10-18 14:34:39 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Galaxy Carder
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7422930589:AAGUnwc6kTDrnBzh3VU3DdfsjPJQuK_3Jnw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Galaxy_Carder")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6847432039")
+
+
+
+
+
+  
